e6d191f01fee4b824f1be5e03b2b89e21ef3a26a
reedry <r.yoshida.c@gmail.com>
2023-07-08 18:53:00 +0900

    generate markdown from input file in args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
-fn main() {
-    let md_input = "Hello, world!";
-    let parser = pulldown_cmark::Parser::new(md_input);
+use std::env::args;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input_file = args().nth(1).expect("No input");
+    let md_input = std::fs::read_to_string(input_file)?;
+    let parser = pulldown_cmark::Parser::new(&md_input);
 
     let mut html_output = String::new();
     pulldown_cmark::html::push_html(&mut html_output, parser);
 
     println!("{}", html_output);
+    Ok(())
 }
