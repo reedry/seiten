807e8581c2848da398cc35f3c36679c44f1da3ec
reedry <r.yoshida.c@gmail.com>
2023-07-08 21:26:47 +0900

    concat current dir

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,22 @@ struct Build {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config_file = std::fs::read_to_string("seiten.toml")?;
     let config: Config = toml::from_str(&config_file)?;
+
+    let input_dir = std::env::current_dir()?.join(config.build.input);
+    let output_dir = std::env::current_dir()?.join(config.build.output);
     println!(
         "input: {}, output: {}",
-        config.build.input, config.build.output
+        input_dir.display(),
+        output_dir.display()
     );
 
-    let md_input = std::fs::read_to_string(config.build.input)?;
+    let md_input = std::fs::read_to_string(input_dir)?;
     let parser = pulldown_cmark::Parser::new(&md_input);
 
     let mut html_output = String::new();
     pulldown_cmark::html::push_html(&mut html_output, parser);
 
-    let mut output = std::fs::File::create(config.build.output)?;
+    let mut output = std::fs::File::create(output_dir)?;
     writeln!(output, "{}", html_output)?;
 
     Ok(())
