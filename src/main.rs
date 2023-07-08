8a209940ce947740faf5dca8cd11421dbf1d308c
reedry <r.yoshida.c@gmail.com>
2023-07-08 18:18:47 +0900

    Use pulldown_cmark

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let md_input = "Hello, world!";
+    let parser = pulldown_cmark::Parser::new(md_input);
+
+    let mut html_output = String::new();
+    pulldown_cmark::html::push_html(&mut html_output, parser);
+
+    println!("{}", html_output);
 }
